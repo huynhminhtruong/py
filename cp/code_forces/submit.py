91c94812678da2f2d686547cee5847aad6950233
huynhminhtruong <huynhminhtruong2003@gmail.com>
2020-10-22 13:06:25 +0700

    Solved codeforces problems.

--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -7,5 +7,15 @@ import functools
 from operator import itemgetter, attrgetter
 
 if __name__ == '__main__':
-    n, k = list(map(int, input().strip().split()))
-    s = sorted(list(map(ord, input().strip())))
+    n = int(input())
+    s = sorted(set(map(int, input().strip().split())))
+    r = 0
+
+    for i in range(1, len(s)):
+        if r == 2:
+            break
+        if s[i] - s[i - 1] == 1:
+            r += 1
+        else:
+            r = 0
+    print("YES" if r == 2 else "NO")
