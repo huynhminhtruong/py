a2eb1332dea51f8f5bd04969b0b5a690aead60ec
huynhminhtruong <huynhminhtruong2003@gmail.com>
2020-10-23 01:01:25 +0700

    Solving codeforces problems.

--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -7,5 +7,16 @@ import functools
 from operator import itemgetter, attrgetter
 
 if __name__ == '__main__':
-    n = int(input())
-    s = list(map(int, input().strip().split()))
+    n, m = list(map(int, input().strip().split()))
+    a = list(map(int, input().strip().split()))
+    d = dict()
+
+    for i in range(m):
+        s = input()
+        if d.get(s) is None:
+            d[s] = 0
+        else:
+            d[s] += 1
+
+    a.sort()
+    sorted_d = { k: v for k, v in sorted(d.items(), key=lambda x: x[1], reverse=True) }
