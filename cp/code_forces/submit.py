90801dfc443ea930d8bc8e2f13dfbb04e9d9df34
huynhminhtruong <huynhminhtruong2003@gmail.com>
2020-10-22 18:13:20 +0700

    Solving codeforces problems.

--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -8,14 +8,45 @@ from operator import itemgetter, attrgetter
 
 if __name__ == '__main__':
     n = int(input())
-    s = sorted(set(map(int, input().strip().split())))
-    r = 0
+    s = list(map(int, input().strip().split()))
+    swap = sorted(s)
+    r, t = 0, 0
 
-    for i in range(1, len(s)):
-        if r == 2:
-            break
-        if s[i] - s[i - 1] == 1:
+    for i in range(n):
+        if swap[i] >= t:
             r += 1
         else:
-            r = 0
-    print("YES" if r == 2 else "NO")
+            t -= swap[i - 1]
+        t += swap[i]
+
+    print(r)
+
+# Little girl Susie went shopping with her mom and she wondered how to improve service quality.
+
+# There are n people in the queue. 
+# For each person we know time ti needed to serve him. 
+# A person will be disappointed if the time he waits is more than the time needed to serve him. 
+# The time a person waits is the total time when all the people who stand in the queue in front of him are served. 
+# Susie thought that if we swap some people in the queue, then we can decrease the number of people who are disappointed.
+
+# Help Susie find out what is the maximum number of not disappointed people can be achieved by swapping people in the queue.
+
+# Input
+# The first line contains integer n (1 ≤ n ≤ 105).
+
+# The next line contains n integers ti (1 ≤ ti ≤ 109), separated by spaces.
+
+# Output
+# Print a single number — the maximum number of not disappointed people in the queue.
+
+# 5
+# 15 2 1 5 3
+# 1 2 3 5 15
+# 4
+
+# 10
+# 13 2 5 55 21 34 1 8 1 3
+# 1 1 2 3 5 8 13 21 34 55
+# 1 1 2 _ _ _ 13 _ 34 55
+
+
