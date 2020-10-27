7bab88ee320b353a0498af025deed891c9159a65
huynhminhtruong <huynhminhtruong2003@gmail.com>
2020-10-27 18:16:03 +0700

    Solving codeforces problems.

--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -7,45 +7,25 @@ import functools
 from operator import itemgetter, attrgetter
 
 if __name__ == '__main__':
-    n, m, k = list(map(int, input().strip().split()))
-    a, b, i = list(map(int, input().strip().split())), 0, 0
+    n = int(input())
+    a, b, c = list(map(int, input().strip().split()))
 
-    a.sort(reverse=True)
+    a.sort()
 
-    if m <= k:
-        print(b)
-    else:
-        while i < len(a):
-            if m <= k:
-                break
-            if k > 0:
-                k -= 1
-            elif m > 0:
-                m += 1
-            m -= a[i]
-            b += 1
-            i += 1
-        print("-1" if m > k else "{}".format(b))
+    # tmp = set('apple')
+    # print(tmp)
 
+# 5
+# 1 2 3 4 5
+# Expected
+# 5
+# 5 4 3 2 1
 
-# 3 5 3
-# 3 1 2
+# 6
+# 1 1 2 2 3 3
+# Expected
+# 5
+# 1 2 3 2 1
 
-# 4 7 2
-# 3 3 2 4
-
-# 5 5 1
-# 1 3 1 2 1
-
-# 5 5 5
-# 1 3 1 2 1
-
-# 5 1 5
-# 1 3 1 2 1
-
-# 5 10 1
-# 4 3 4 2 4
-
-# 5 50 6
-# 2 1 3 1 3
-# -1
+# 1 1 2 2 3 4 5
+# 1 2 5 4 3 2 1
