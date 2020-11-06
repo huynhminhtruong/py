2188f8bc1371a017fb1897389a0d974f5153088a
huynhminhtruong <huynhminhtruong2003@gmail.com>
2020-11-06 13:20:16 +0700

    Solved codeforces problems 1216-B.

--- cp/code_forces/problems.py
+++ cp/code_forces/problems.py
@@ -285,6 +285,19 @@ def _1144B_Parity_Alternated_Deletions_By_Built_In_Pop_List():
         odd.pop()
     print("{}".format(sum(even) + sum(odd)))
 
+def _1216B_Shooting():
+    Y = lambda: list(map(int, input().split()))
+    N = lambda: int(input())
+
+    n = N()
+    a = Y()
+    r = 0
+    a = [(-a[k], k % len(a) + 1) for k in range(n)]
+    a.sort()
+    for i in range(n):
+        r += -a[i][0] * i + 1
+    print("{0}\n{1}".format(r, " ".join(map(str, [x[1] for x in a]))))
+
 if __name__ == '__main__':
     n = list(map(int, input().strip().split()))
     c = 0
--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -10,19 +10,12 @@ from collections import Counter
 if __name__ == '__main__':
     Y = lambda: list(map(int, input().split()))
     N = lambda: int(input())
-    even, odd = list(), list()
 
     n = N()
     a = Y()
+    r = 0
+    a = [(-a[k], k % len(a) + 1) for k in range(n)]
     a.sort()
-
-    for _ in a:
-        even.append(_) if _ % 2 == 0 else odd.append(_)
-    while len(even) and len(odd):
-        even.pop()
-        odd.pop()
-    if len(even):
-        even.pop()
-    if len(odd):
-        odd.pop()
-    print("{}".format(sum(even) + sum(odd)))
+    for i in range(n):
+        r += -a[i][0] * i + 1
+    print("{0}\n{1}".format(r, " ".join(map(str, [x[1] for x in a]))))
