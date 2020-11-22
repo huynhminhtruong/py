1a4669f302135a83411bd3f0b9f4b047b7e855c8
huynhminhtruong <huynhminhtruong2003@gmail.com>
2020-11-22 23:36:36 +0700

    Solved codeforces problems 66-B.

--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -11,3 +11,23 @@ if __name__ == '__main__':
     Y = lambda: list(map(int, input().split()))
     P = lambda: map(int, input().split())
     N = lambda: int(input())
+
+    n = N()
+    a = Y()
+    l, r, down, MAX = 0, 1, False, 0
+
+    for i in range(1, n):
+        if a[i] > a[i - 1]:
+            if not down:
+                r += 1
+            else:
+                if r > MAX:
+                    MAX = r
+                r = i - l + 1
+                down = False
+        else:
+            if a[i] < a[i - 1]:
+                l = i
+            r += 1
+            down = True
+    print(r if r > MAX else MAX)
