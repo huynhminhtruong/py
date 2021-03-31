e0e6164edf35c9b2dcf5fa8eb5c72af3b5850ae1
huynhminhtruong <huynhminhtruong2003@gmail.com>
2021-03-31 16:59:09 +0700

    Solved codeforces problems and preparing interview.

--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -1,4 +1,30 @@
+from collections import Counter
+from itertools import combinations
+
+def is_subsequences(s_1, s_2) -> bool:
+    i = j = 0
+    while i < len(s_1) and j < len(s_2):
+        if s_1[i] == s_2[j]:
+            i += 1
+        j += 1
+    return i == len(s_1)
+
 if __name__ == '__main__':
-    x, y = map(int, input().split())
-    y -= 1
-    print("YNEOS"[y < 0 or y > x or (y == 0 and x != 0) or (x - y) % 2::2])
+    q = int(input())
+
+    while q > 0:
+        s, t, p = input(), input(), input()
+
+        if not is_subsequences(s, t):
+            print("NO")
+        else:
+            s, t, p = Counter(s), Counter(t), Counter(p)
+
+            for k in t:
+                if not t.get(k) <= s.get(k, 0) + p.get(k, 0):
+                    print("NO")
+                    break
+            else:
+                print("YES")
+
+        q -= 1
