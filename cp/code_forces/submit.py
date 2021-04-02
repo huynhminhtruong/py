9564ab88597a56568423fec15ebf4b75fbf91024
huynhminhtruong <huynhminhtruong2003@gmail.com>
2021-04-02 15:21:28 +0700

    Solved codeforces problems and preparing interviews.

--- cp/code_forces/submit.py
+++ cp/code_forces/submit.py
@@ -1,14 +1,22 @@
 if __name__ == '__main__':
-    s = list()
-    for _ in range(4):
-        s.append(len(input()) - 2)
-    l = sorted(s)
-    mn, mx = l[0], l[3]
-    mn = l[0] if l[0] <= l[1] / 2 else 0
-    mx = l[3] if l[3] >= l[2] * 2 else 0
-    if not mn and mx:
-        print(chr(s.index(mx) + ord("A")))
-    elif mn and not mx:
-        print(chr(s.index(mn) + ord("A")))
-    else:
-        print("C")
+    h, a = input(), input()
+    s = lambda x, n, m: "%s %s %s" % (x, n, m)
+    t = int(input())
+    b, c = dict(), dict()
+
+    def is_red(d, key, name, m, func):
+        if d[key] in (2, 3):
+            print(func(name, key, m))
+
+    while t > 0:
+        d = input().split()
+        cnt = 1 if d[3] == "y" else 2
+        if d[1] == "h":
+            if b.get(d[2], 0) < 2 and 1 < b.get(d[2], 0) + cnt < 4:
+                print(s(h, d[2], d[0]))
+            b[d[2]] = b.get(d[2], 0) + cnt
+        else:
+            if c.get(d[2], 0) < 2 and 1 < c.get(d[2], 0) + cnt < 4:
+                print(s(a, d[2], d[0]))
+            c[d[2]] = c.get(d[2], 0) + cnt
+        t -= 1
